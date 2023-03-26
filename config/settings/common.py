2c92cb1c8668534c58513323b5956fe078a60d6a
ja153903 <jaimeabbariao@gmail.com>
2023-03-26 13:31:11 -0400

    feat: updating so that server runs

--- config/settings/common.py
+++ config/settings/common.py
@@ -14,8 +14,7 @@ from pathlib import Path
 
 from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 ENVIRONMENT = config("ENVIRONMENT", default="development")
