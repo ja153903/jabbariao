489ae8deb240f276a5922fbf0a20e99cf9c17ea8
ja153903 <jaimeabbariao@gmail.com>
2023-03-26 11:20:08 -0400

    feat: updating with default settings

--- config/settings/common.py
+++ config/settings/common.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "jabbariao.placeholder",
     "jabbariao.posts",
 ]
 
@@ -44,6 +45,11 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
 
 ROOT_URLCONF = "config.urls"
 
