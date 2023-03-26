18da56233aac1b6e051f5aa19d8e19ecfc33316f
ja153903 <jaimeabbariao@gmail.com>
2023-03-26 19:18:20 -0400

    feat: updating

--- jabbariao/core/models.py
+++ jabbariao/core/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class BaseModel(models.Model):
+    """
+    BaseModel is an abstract Django model that includes
+    basic fields for all models to inherit.
+    """
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
