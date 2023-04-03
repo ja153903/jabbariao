8eb33e5e55250d061f2ad8d97704086531b195ab
ja153903 <jaimeabbariao@gmail.com>
2023-04-02 21:22:16 -0400

    feat: updating with proper API for blog endpoint

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from jabbariao.blog.views import PostViewSet
+
+router = routers.DefaultRouter()
+router.register(r"posts", PostViewSet, basename="post")
 
 urlpatterns = [
+    path("api/v1", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("admin/", admin.site.urls),
 ]
--- jabbariao/blog/views.py
+++ jabbariao/blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Post
+from .serializers import PostListSerializer, PostDetailSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+    detail_serializer_class = PostDetailSerializer
