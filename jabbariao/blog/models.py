d4a444a406c89ebdfb1cf82f839c4531178cfa78
ja153903 <jaimeabbariao@gmail.com>
2023-04-03 00:41:38 -0400

    feat: adding published field

--- jabbariao/blog/models.py
+++ jabbariao/blog/models.py
@@ -7,6 +7,7 @@ class Post(BaseModel):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=140, null=True, blank=True)
     content = models.TextField(null=True)
+    published = models.BooleanField(default=False)
 
     class Meta:
         indexes = [models.Index(fields=["title"], name="title_index")]
