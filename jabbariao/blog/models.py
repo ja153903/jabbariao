f1ca5ab2cf8ed8f15ed7c67a7b14c4709b76249d
ja153903 <jaimeabbariao@gmail.com>
2023-04-03 00:32:33 -0400

    feat: adding description field

--- jabbariao/blog/models.py
+++ jabbariao/blog/models.py
@@ -5,6 +5,7 @@ from jabbariao.core.models import BaseModel
 
 class Post(BaseModel):
     title = models.CharField(max_length=255)
+    description = models.CharField(max_length=140, null=True, blank=True)
     content = models.TextField(null=True)
 
     class Meta:
--- jabbariao/blog/serializers.py
+++ jabbariao/blog/serializers.py
@@ -6,7 +6,7 @@ from .models import Post
 class PostListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ("id", "title", "created_at", "updated_at")
+        fields = ("id", "title", "created_at")
 
 
 class PostDetailSerializer(serializers.ModelSerializer):
